d421752475360453439f3b01965f3f9df7784fc7
Nikton25 <gerardo.benitez@davinci.edu.ar>
2026-05-05 22:33:23 -0300

    Comenzamos el desarrollo del Menu Principal utilizando un switch con las opciones a las que el sistema podra acceder

--- Sistema_Garage/src/Main.java
+++ Sistema_Garage/src/Main.java
@@ -1,23 +1,38 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
+    Scanner scan = new Scanner(System.in);
+    Garage gar = new Garage(50);
+    int opcion = 0;
 
-    Garage gar = new Garage(4);
+    System.out.println("=== SISTEMA DE GARAGE ===");
 
-    Auto Focus = new Auto("Ford", "Focus", "iob471", 5);
-    Auto Chevi = new Auto("Chevi", "Chevi", "iob471", 5);
+    while(opcion!=0){
+        System.out.println("\n1. Registrar ingreso");
+        System.out.println("2. Registrar salida");
+        System.out.println("3. Listar vehiculos");
+        System.out.println("4. Estado del garage");
+        System.out.println("5. Reportes");
+        System.out.println("6. Salir");
 
-    try {gar.registrarEntrada(Chevi);}
-    catch (HorasInvalidasException | PatenteDuplicadaException | GarageLlenoException e) {
-        System.out.println(e.getMessage());
-    }
+        opcion = scan.nextInt();
+        scan.nextLine();
 
-    gar.mostrarVehiculosEstacionados();
-
-    try {gar.registrarSalida("iob984");}
-    catch (VehiculoNoEncontradoException e) {
-        System.out.println(e.getMessage());
+        switch(opcion){
+            case 1:
+                try {
+                    System.out.println("Ingresa el tipo de vehiculo");
+                    String tipoVehiculo = scan.nextLine();
+                    System.out.println("Ingresa la marca");
+                    String marca = scan.nextLine();
+                    System.out.println("Ingresa el modelo");
+                    String modelo = scan.nextLine();
+                    System.out.println("Ingresa la patente");
+                    String patente = scan.nextLine();
+                }
+        }
     }
 
 
